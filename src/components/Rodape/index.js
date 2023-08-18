92b6d1d2b577c4978e11b8108ef38873f2f945f9
Amanda <86625044+4m4nd4@users.noreply.github.com>
2023-08-18 15:05:05 -0300

    Update no rodape

--- src/components/Rodape/index.js
+++ src/components/Rodape/index.js
@@ -3,7 +3,7 @@ import styles from './Rodape.module.css';
 function Rodape() {
     return (
         <footer className={styles.rodape}>
-            <h2>Desenvolvido por Alura.</h2>
+            <h2>Desenvolvido por Amandah2>
         </footer>
     )
 }
